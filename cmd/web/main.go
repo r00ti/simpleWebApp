27aa962b9bcedd3a63820a1df83459db962c34fc
Adrian Rutkowski <rutkowski.adr@gmail.com>
2022-12-08 09:41:04 +0100

    directory refactor

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/r00ti/simpleWebApp/pkg/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
